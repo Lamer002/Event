1d9dda6dae82f8653704fedbf2fc8426de5cbd78
Lamer002 <vandreev02@mail.ru>
2025-08-28 15:10:53 +0300

    merge

--- Event.h
+++ Event.h
@@ -3,14 +3,15 @@
 #include <string>
 #include <chrono>
 
-class Event {
+class Event
+{
 public:
-    Event(std::chrono::sys_days date, const std::string& description);
+    Event(std::chrono::sys_days date, const std::string &description);
 
     std::chrono::sys_days getDate() const;
     std::string getDescription() const;
 
-    bool operator<(const Event& other) const;
+    bool operator<(const Event &other) const;
 
 private:
     std::chrono::sys_days date_;
